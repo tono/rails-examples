61141743f8960f26ee6b93d6d92ac1ab2742a19c
Tetsuya Ono <tetsuya.ono@gmail.com>
2021-12-27 08:47:20 +0900

    add base appliction

---
+++ engine-example/config/initializers/generators.rb
@@ -0,0 +1,14 @@
+Rails.application.config.generators do |g|
+  g.javascripts false
+  g.stylesheets false
+  g.helper false
+  g.jbuilder false
+  g.test_framework :rspec,
+    fixture: true,
+    view_specs: false,
+    helper_specs: false,
+    routing_specs: false,
+    controller_specs: false,
+    request_specs: true
+  g.fixture_replacement :factory_bot, dir: 'spec/factories'
+end
